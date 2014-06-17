a50ff0b5392176680725a3d60d0780400d4e7458
Thomas Lim <txl11@ic.ac.uk>
2014-06-17 18:00:06 +0100

    Parsing pretty much everything we need now

--- parser.py
+++ parser.py
@@ -1,7 +1,9 @@
 import string
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+import tinycss
+
 
 url = "http://www.theguardian.com/crosswords/cryptic/26288"
 
@@ -29,14 +31,60 @@ down_clues = {}
 
 for li in raw_across.findAll('li'):
     clue = li.text[5:]
-    across_clues[li.span.text] = string.strip(clue)
+    across_clues[li.span.text] = {}
+    across_clues[li.span.text]['clue'] = string.strip(clue)
 
 
 for li in raw_down.findAll('li'):
     clue = li.text[5:]
-    down_clues[li.span.text] = string.strip(clue)
+    down_clues[li.span.text] = {}
+    down_clues[li.span.text]['clue'] = string.strip(clue)
 
-print across_clues
-print down_clues
 
-print raw_layout
+square_size = 29
+
+for div in raw_layout.findAll('div', {'class': 'across'}):
+
+    parser = tinycss.make_parser()
+    stylesheet = parser.parse_style_attr(div['style'])
+
+    for dec in stylesheet[0]:
+        
+        if dec.name == "left":
+            left = dec.value[0].value / square_size
+        elif dec.name == "top":
+            top = dec.value[0].value / square_size
+        else:
+            pass
+
+    position = left + top*15
+    across_clues[div['id'].split('-')[0]]['launch_spot'] = position
+
+
+
+
+for line in str(raw_answers.script).split("\n"):
+    if "solutions[" in line:
+        separated_answer = line.split('"')
+
+        key = separated_answer[1].split('-')
+        res = separated_answer[3]
+
+
+        # This is reliant on the order of the answers going down the page
+        # If not, we have problems. More code would be needed
+        try:
+            if key[1] == 'across':
+                across_clues[key[0]]['answer'] += res
+            elif key[1] == 'down':
+                down_clues[key[0]]['answer'] += res
+
+        except KeyError:
+            if key[1] == 'across':
+                across_clues[key[0]]['answer'] = res
+            elif key[1] == 'down':
+                down_clues[key[0]]['answer'] = res
+            
+
+
+print across_clues, down_clues
