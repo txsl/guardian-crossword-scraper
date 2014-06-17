655344bcc149166085606f574474c2d3b4933025
Thomas Lim <txl11@ic.ac.uk>
2014-06-17 18:45:43 +0100

    Looking good at parsing and producing XML now. Also fixed a bug with muliple clue IDs given in one clue

--- parser.py
+++ parser.py
@@ -3,9 +3,10 @@ import string
 import requests
 from bs4 import BeautifulSoup
 import tinycss
+from lxml import etree
 
 
-url = "http://www.theguardian.com/crosswords/cryptic/26288"
+url = "http://www.theguardian.com/crosswords/cryptic/26260"
 
 r = requests.get(url)
 
@@ -37,8 +38,9 @@ for li in raw_across.findAll('li'):
 
 for li in raw_down.findAll('li'):
     clue = li.text[5:]
-    down_clues[li.span.text] = {}
-    down_clues[li.span.text]['clue'] = string.strip(clue)
+    clue_id = li.span.text.split(",")[0]
+    down_clues[clue_id] = {}
+    down_clues[clue_id]['clue'] = string.strip(clue)
 
 
 square_size = 29
@@ -59,6 +61,24 @@ for div in raw_layout.findAll('div', {'class': 'across'}):
 
     position = left + top*15
     across_clues[div['id'].split('-')[0]]['launch_spot'] = position
+
+for div in raw_layout.findAll('div', {'class': 'down'}):
+
+    parser = tinycss.make_parser()
+    stylesheet = parser.parse_style_attr(div['style'])
+
+    for dec in stylesheet[0]:
+        
+        if dec.name == "left":
+            left = dec.value[0].value / square_size
+        elif dec.name == "top":
+            top = dec.value[0].value / square_size
+        else:
+            pass
+
+    position = left + top*15
+    print down_clues
+    down_clues[div['id'].split('-')[0]]['launch_spot'] = position
 
 
 
@@ -88,3 +108,69 @@ for line in str(raw_answers.script).split("\n"):
 
 
 print across_clues, down_clues
+
+
+root = etree.Element('crossword')
+
+child = etree.Element('Title')
+child.attrib['v'] = "This title doesn't appear"
+root.append(child)
+
+child = etree.Element('Author')
+child.attrib['v'] = "AUTHOR"
+root.append(child)
+
+child = etree.Element('Category')
+child.attrib['v'] = "The Guardian Crosswords"
+root.append(child)
+
+child = etree.Element('Copyright')
+child.attrib['v'] = "The Guardian"
+root.append(child)
+
+child = etree.Element('Editor')
+child.attrib['v'] = "EDITOR"
+root.append(child)
+
+child = etree.Element('Width')
+child.attrib['v'] = "15"
+root.append(child)
+
+child = etree.Element('Height')
+child.attrib['v'] = "15"
+root.append(child)
+
+
+across_child = etree.Element('across')
+
+i = 1
+for key, value in across_clues.iteritems():
+    child = etree.Element('a' + str(i))
+    child.attrib['a'] = value['answer']
+    child.attrib['c'] = value['clue']
+    child.attrib['n'] = str(value['launch_spot'])
+    child.attrib['cn'] = key
+    across_child.append(child)
+
+    i += 1
+
+root.append(across_child)
+
+
+down_child = etree.Element('down')
+
+i = 1
+for key, value in down_clues.iteritems():
+    child = etree.Element('d' + str(i))
+    child.attrib['a'] = value['answer']
+    child.attrib['c'] = value['clue']
+    child.attrib['n'] = str(value['launch_spot'])
+    child.attrib['cn'] = key
+    down_child.append(child)
+
+    i += 1
+
+root.append(down_child)
+
+s = etree.tostring(root, pretty_print=True)
+print s
