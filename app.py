dae6804d318bd977ebb66b057dc56e974771e35c
Thomas Lim <txl11@ic.ac.uk>
2014-06-18 12:31:02 +0100

    Now caching the responses (no accidental DDOS of the guardian), and returning a 404 if crossword not found

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, Response
-
 from parser import get_crossword
 
 app = Flask(__name__)
 
+cached = {}
+
 @app.route('/guardiancross/<type>/<id>')
 def return_guardian_crossword(type, id):
-    
-    data = get_crossword(id, type)
-    
+
+    try:
+        data = cached[type][id]
+    except KeyError:
+
+        data = get_crossword(id, type)
+
+        if data is None:
+            return 'Crossword not found', 404
+
+        try:
+            cached[type][id] = data
+        except KeyError:
+            cached[type] = {}
+            cached[type][id] = data
+
+    print cached
     return Response(data, mimetype='text/xml')
 
 app.run()
